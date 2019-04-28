678c3270ead72c5c3db286ab607d7cf35cd6ad28
Insung Jung <superisis@gmail.com>
2019-04-28 17:46:52 +0900

    스피너 외부 라이브러리 사용 https://github.com/jaredrummler/MaterialSpinner

--- app/src/main/java/kr/insungjung/pizzaorderapp_ex/StoreDetailActivity.java
+++ app/src/main/java/kr/insungjung/pizzaorderapp_ex/StoreDetailActivity.java
@@ -2,6 +2,9 @@ package kr.insungjung.pizzaorderapp_ex;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
+
+import com.jaredrummler.materialspinner.MaterialSpinner;
 
 public class StoreDetailActivity extends AppCompatActivity {
 
@@ -9,5 +12,14 @@ public class StoreDetailActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_store_detail);
+
+        MaterialSpinner spinner = findViewById(R.id.menuSpinner);
+        spinner.setItems("Ice Cream Sandwich", "Jelly Bean", "KitKat", "Lollipop", "Marshmallow");
+        spinner.setOnItemSelectedListener(new MaterialSpinner.OnItemSelectedListener<String>() {
+
+            @Override public void onItemSelected(MaterialSpinner view, int position, long id, String item) {
+                Toast.makeText(StoreDetailActivity.this, "스피너 클릭", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
