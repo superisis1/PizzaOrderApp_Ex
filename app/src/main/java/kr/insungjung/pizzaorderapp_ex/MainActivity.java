1373eb130a3e1169cf011435508166065a11ce7c
Insung Jung <superisis@gmail.com>
2019-04-28 15:51:15 +0900

    스토어 리스트뷰 xml 완성

--- app/src/main/java/kr/insungjung/pizzaorderapp_ex/MainActivity.java
+++ app/src/main/java/kr/insungjung/pizzaorderapp_ex/MainActivity.java
@@ -2,12 +2,29 @@ package kr.insungjung.pizzaorderapp_ex;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Adapter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import kr.insungjung.pizzaorderapp_ex.adapter.StoreAdapter;
+import kr.insungjung.pizzaorderapp_ex.databinding.ActivityMainBinding;
+import kr.insungjung.pizzaorderapp_ex.datas.Store;
 
 public class MainActivity extends AppCompatActivity {
+
+    StoreAdapter mStoreAdapter;
+
+    List<Store> storeList = new ArrayList<>();
+
+    ActivityMainBinding act;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
     }
 }
--- app/src/main/java/kr/insungjung/pizzaorderapp_ex/datas/Store.java
+++ app/src/main/java/kr/insungjung/pizzaorderapp_ex/datas/Store.java
@@ -1,4 +1,19 @@
 package kr.insungjung.pizzaorderapp_ex.datas;
 
-public class Store {
+import java.io.Serializable;
+
+public class Store implements Serializable {
+
+    public String storeNameTxt;
+    public String openTimeTxt;
+    public String phoneNumTxt;
+    public String menuTxt;
+
+    public Store(String storeNameTxt, String openTimeTxt, String phoneNumTxt, String menuTxt) {
+        this.storeNameTxt = storeNameTxt;
+        this.openTimeTxt = openTimeTxt;
+        this.phoneNumTxt = phoneNumTxt;
+        this.menuTxt = menuTxt;
+
+    }
 }
