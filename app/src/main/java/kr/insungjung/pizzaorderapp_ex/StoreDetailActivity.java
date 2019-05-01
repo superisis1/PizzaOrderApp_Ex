f2107e19980a823de954b8a5587aa54722bc2088
Insung Jung <superisis@gmail.com>
2019-05-01 11:46:58 +0900

    리사이클러뷰 카드아이템 중앙정렬

--- app/src/main/java/kr/insungjung/pizzaorderapp_ex/StoreDetailActivity.java
+++ app/src/main/java/kr/insungjung/pizzaorderapp_ex/StoreDetailActivity.java
@@ -220,22 +220,20 @@ public class StoreDetailActivity extends BaseActivity {
     }
 
     void fillPizzas() {
-        pizza.add(new Pizza("https://upload.wikimedia.org/wikipedia/en/thumb/d/d2/Pizza_Hut_logo.svg/220px-Pizza_Hut_logo.svg.png","페페로니", 20000));
-        pizza.add(new Pizza("https://upload.wikimedia.org/wikipedia/en/thumb/d/d2/Pizza_Hut_logo.svg/220px-Pizza_Hut_logo.svg.png","치즈피자", 18000));
-        pizza.add(new Pizza("https://upload.wikimedia.org/wikipedia/en/thumb/d/d2/Pizza_Hut_logo.svg/220px-Pizza_Hut_logo.svg.png","하와이안", 24000));
-        pizza.add(new Pizza("https://upload.wikimedia.org/wikipedia/en/thumb/d/d2/Pizza_Hut_logo.svg/220px-Pizza_Hut_logo.svg.png","버섯피자", 20000));
-        pizza.add(new Pizza("https://upload.wikimedia.org/wikipedia/en/thumb/d/d2/Pizza_Hut_logo.svg/220px-Pizza_Hut_logo.svg.png","시금치피자", 24000));
-        pizza.add(new Pizza("https://upload.wikimedia.org/wikipedia/en/thumb/d/d2/Pizza_Hut_logo.svg/220px-Pizza_Hut_logo.svg.png","베이컨체다치즈 피자", 34000));
-        pizza.add(new Pizza("https://upload.wikimedia.org/wikipedia/en/thumb/d/d2/Pizza_Hut_logo.svg/220px-Pizza_Hut_logo.svg.png","우리고구마", 20000));
-        pizza.add(new Pizza("https://upload.wikimedia.org/wikipedia/en/thumb/d/d2/Pizza_Hut_logo.svg/220px-Pizza_Hut_logo.svg.png","콰트로 치즈 퐁듀", 26000));
-        pizza.add(new Pizza("https://upload.wikimedia.org/wikipedia/en/thumb/d/d2/Pizza_Hut_logo.svg/220px-Pizza_Hut_logo.svg.png","슈퍼디럭스", 22000));
-        pizza.add(new Pizza("https://upload.wikimedia.org/wikipedia/en/thumb/d/d2/Pizza_Hut_logo.svg/220px-Pizza_Hut_logo.svg.png","불고기", 22000));
-        pizza.add(new Pizza("https://upload.wikimedia.org/wikipedia/en/thumb/d/d2/Pizza_Hut_logo.svg/220px-Pizza_Hut_logo.svg.png","더블크러스트 이베리코", 36000));
-        pizza.add(new Pizza("https://upload.wikimedia.org/wikipedia/en/thumb/d/d2/Pizza_Hut_logo.svg/220px-Pizza_Hut_logo.svg.png","블랙앵거스 스테이크", 32000));
-        pizza.add(new Pizza("https://upload.wikimedia.org/wikipedia/en/thumb/d/d2/Pizza_Hut_logo.svg/220px-Pizza_Hut_logo.svg.png","글램핑 바베큐", 28000));
-        pizza.add(new Pizza("https://upload.wikimedia.org/wikipedia/en/thumb/d/d2/Pizza_Hut_logo.svg/220px-Pizza_Hut_logo.svg.png","블랙타이거 슈림프", 36000));
-        pizza.add(new Pizza("https://upload.wikimedia.org/wikipedia/en/thumb/d/d2/Pizza_Hut_logo.svg/220px-Pizza_Hut_logo.svg.png","와규 앤 비스테카", 34000));
-        pizza.add(new Pizza("https://upload.wikimedia.org/wikipedia/en/thumb/d/d2/Pizza_Hut_logo.svg/220px-Pizza_Hut_logo.svg.png","직화 스테이크", 29000));
+        pizza.add(new Pizza("https://cdn.dominos.co.kr/admin/upload/goods/20180827_TD5SQjfJ.jpg","페페로니", 20000));
+        pizza.add(new Pizza("https://cdn.dominos.co.kr/admin/upload/goods/20180827_m1G6UO53.jpg","치즈피자", 18000));
+        pizza.add(new Pizza("https://cdn.dominos.co.kr/admin/upload/goods/20180827_03clpqNq.jpg","하와이안", 24000));
+        pizza.add(new Pizza("https://cdn.dominos.co.kr/admin/upload/goods/20180827_hodWdX2e.jpg","베이컨체다치즈", 34000));
+        pizza.add(new Pizza("https://cdn.dominos.co.kr/admin/upload/goods/20190131_w8469814.jpg","우리고구마", 20000));
+        pizza.add(new Pizza("https://cdn.dominos.co.kr/admin/upload/goods/20180827_w1WcPp2o.jpg","콰트로 치즈 퐁듀", 26000));
+        pizza.add(new Pizza("https://cdn.dominos.co.kr/admin/upload/goods/20180827_WqsN9t16.jpg","슈퍼디럭스", 22000));
+        pizza.add(new Pizza("https://cdn.dominos.co.kr/admin/upload/goods/20180827_7v9YHcEK.jpg","불고기", 22000));
+        pizza.add(new Pizza("https://cdn.dominos.co.kr/admin/upload/goods/20190226_R7d7qxaL.jpg","더블크러스트 이베리코", 36000));
+        pizza.add(new Pizza("https://cdn.dominos.co.kr/admin/upload/goods/20190226_WtefcsQd.jpg","블랙앵거스 스테이크", 32000));
+        pizza.add(new Pizza("https://cdn.dominos.co.kr/admin/upload/goods/20181116_DwpeAY8n.jpg","글램핑 바베큐", 28000));
+        pizza.add(new Pizza("https://cdn.dominos.co.kr/admin/upload/goods/20180827_6UbajoYY.jpg","블랙타이거 슈림프", 36000));
+        pizza.add(new Pizza("https://cdn.dominos.co.kr/admin/upload/goods/20180827_ji6cyfry.jpg","와규 앤 비스테카", 34000));
+        pizza.add(new Pizza("https://cdn.dominos.co.kr/admin/upload/goods/20180827_ZIna5r8b.jpg","직화 스테이크", 29000));
     }
 
 }
